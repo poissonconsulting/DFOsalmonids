2a1ad513f2c1ae837c80852bf77da70c6664912c
John Doe <johndoe@poissonconsulting.ca>
2024-01-04 14:41:07 -0800

    package skeleton

---
+++ README.Rmd
@@ -0,0 +1,19 @@
+
+# DFOsalmonids
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of DFOsalmonids is to ...
+
+## Installation
+
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(DFOsalmonids)
+## basic example code
+```
---
+++ scripts/build.R
@@ -0,0 +1,18 @@
+roxygen2md::roxygen2md()
+
+styler::style_pkg(
+  scope = "line_breaks",
+  filetype = c("R", "Rmd")
+)
+
+lintr::lint_package(linters = linters_with_defaults(
+  line_length_linter = line_length_linter(1000))
+)
+lintr::lint_package()
+
+devtools::test()
+devtools::document()
+
+pkgdown::build_home()
+
+devtools::check()
