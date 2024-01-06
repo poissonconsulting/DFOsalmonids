8b9fae2384b33ecd38a65587dfc053a216788c19
John Doe <johndoe@poissonconsulting.ca>
2024-01-05 16:34:04 -0800

    basic package structure set and package functional

---
+++ R/scale-age.R
@@ -0,0 +1,13 @@
+#' scale age data
+#'
+#' Example data for counts of salmonids from different age classes based on
+#' scale data
+#'
+#' @format ## `scale_age`
+#' A data frame with 112 rows and 3 columns:
+#' \describe{
+#'   \item{brood_year}{Brood year}
+#'   \item{captures}{Number of samples for that age group}
+#'   \item{age}{Age of fish based on scale sample}
+#' }
+"scale_age"
--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -1,4 +1,17 @@
-## code to prepare `salmonids` dataset goes here
+scale_age <- readxl::read_xlsx(
+  "~/Poisson Consulting Dropbox/Data/sample-size-23/Scale Age Data Set - Copy.xlsx",
+  range = "A2:E30"
+) |>
+  (function(x) {
+    cbind(
+      x[1],
+      stack(
+        x,
+        select = 2:5
+      )
+    )
+  })() |>
+  setNames(c("brood_year", "captures", "age"))
 
 # use expanded sheet since more data (TotCatch has 189 more non-NA data points)
 salmonids <- readxl::read_xlsx(
@@ -49,4 +62,5 @@ salmonids <- readxl::read_xlsx(
     )
   )
 
+usethis::use_data(scale_age, overwrite = TRUE)
 usethis::use_data(salmonids, overwrite = TRUE)
